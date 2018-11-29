604402739223334cf221121cf96fe0d3d7282e52
LegitimatePotato <decklynkern@gmail.com>
2018-11-29 10:18:04 -0700

    Update __init__.py

--- console/editor/__init__.py
+++ console/editor/__init__.py
@@ -1,4 +1,5 @@
 from console import *
+import pprint
 class Editor(ConsoleWindow):
     ups=60
     activeChar=StringVar()
@@ -32,6 +33,42 @@ class Editor(ConsoleWindow):
                 tkColour="#%02x%02x%02x"%self.colours[colour]
                 self.widget(tk.Radiobutton,c+1,0,top=self.fgs,text=colour,variable=self.activeFG,value=colour,bg=tkColour,**settings)
                 self.widget(tk.Radiobutton,c+1,0,top=self.bgs,text=colour,variable=self.activeBG,value=colour,bg=tkColour,**settings)
+        maxDist=math.hypot(24,79)
+        ratios=(
+            (" ",0),
+            ("░",1/3),
+            ("▒",1/2),
+            ("▓",2/3),
+        )
+        colours=(
+            ("0",0),
+            ("8",.5),
+            ("7",.75),
+            ("F",1),
+        )
+        values=[]
+        gradient=[]
+        for fg,fgr in colours:
+            for bg,bgr in colours:
+                for char,ratio in ratios:
+                    value=ratio*fgr+(1-ratio)*bgr
+                    for v in values:
+                        if abs(value-v)<0.01:
+                            break
+                    else:
+                        values.append(value)
+                        gradient.append((value,char,fg,bg))
+        gradient.sort(key=lambda x:x[0])
+        pprint.pprint(gradient)
+        for x in range(80):
+            for y in range(25):
+                value=math.hypot(x,y)/maxDist
+                for grad in range(len(gradient)):
+                    if value<gradient[grad][0]:
+                        self.display[x,y]=gradient[grad-1][1:]
+                        break
+                else:
+                    self.display[x,y]=gradient[-1][1:]
     def changeChar(self,char):
         def inner():
             self.activeChar=char
